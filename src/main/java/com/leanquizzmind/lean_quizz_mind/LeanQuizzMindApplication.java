3f19578316cd56fc69f6b8cd354035b29538a242
Fernando Perera <fernando02perera@gmail.com>
2023-04-27 12:19:23 +0200

    initial settings applied.

---
+++ src/main/java/com/leanquizzmind/lean_quizz_mind/LeanQuizzMindApplication.java
@@ -0,0 +1,13 @@
+package com.leanquizzmind.lean_quizz_mind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeanQuizzMindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeanQuizzMindApplication.class, args);
+	}
+
+}
