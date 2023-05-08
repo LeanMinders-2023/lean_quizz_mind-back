0b0507f0813bd30e836c8eb8cbdb569f800151c4
Fernando Perera <fernando02perera@gmail.com>
2023-05-08 11:30:39 +0200

    testing(Text): should_return_new_text_if_text_is_not_empty test failed

--- src/test/java/com/leanquizzmind/lean_quizz_mind/domain/valueObjects/TextTest.java
+++ src/test/java/com/leanquizzmind/lean_quizz_mind/domain/valueObjects/TextTest.java
@@ -16,7 +16,15 @@ class TextTest {
     void should_return_error_response_if_string_is_empty() {
 
         assertThrows(IllegalArgumentException.class, () -> Text.createText(""));
+    }
 
+    @Test
+    void should_return_new_text_if_text_is_not_empty() {
+
+        Text text = Text.createText("example");
+        String expectedResponse = "example";
+
+        assertEquals(text.getText(), expectedResponse);
     }
 
 }
