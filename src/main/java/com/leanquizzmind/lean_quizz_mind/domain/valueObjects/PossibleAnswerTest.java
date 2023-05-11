1ec1ec3e6a4a7597f87af3a52051bf8858d370ff
Fernando Perera <fernando02perera@gmail.com>
2023-05-11 16:15:21 +0200

    testing(PossibleAnswer): should_pass_an_error_response_that_there_cant_be_two_correct_asnwers test failed

--- src/main/java/com/leanquizzmind/lean_quizz_mind/domain/valueObjects/PossibleAnswerTest.java
+++ src/main/java/com/leanquizzmind/lean_quizz_mind/domain/valueObjects/PossibleAnswerTest.java
@@ -13,10 +13,12 @@ public class PossibleAnswerTest {
      *   List<Answer> [answer1, answer2...]                 ->      new PossibleAnswer
      *   List<Answer> []                                    ->      return error response
      *   List<Answer> [answer1(true), answer2(true)...]     ->      return error response
+     *   List<Answer> [answer1(false), answer2(false)...]     ->      return error response
     */
 
     Text firstText = Text.createText("example 1");
     Text secondText = Text.createText("example 2");
+    Text thirdText = Text.createText("example 3");
 
     @Test
     void should_create_a_new_possible_answer() {
@@ -34,6 +36,16 @@ public class PossibleAnswerTest {
         List<Answer> answerList = List.of();
 
         assertThrows(NullPointerException.class, () -> PossibleAnswer.createPossibleAnswer(answerList));
+    }
+
+    @Test
+    void should_pass_an_error_response_that_there_cant_be_two_correct_asnwers() {
+        Answer firstAnswer = new Answer(firstText, true);
+        Answer secondAnswer = new Answer(secondText, true);
+        Answer thirdAnswer = new Answer(thirdText, false);
+        List<Answer> answerList = List.of(firstAnswer, secondAnswer, thirdAnswer);
+
+        assertThrows(IllegalArgumentException.class, () -> PossibleAnswer.createPossibleAnswer(answerList));
     }
 
 }
