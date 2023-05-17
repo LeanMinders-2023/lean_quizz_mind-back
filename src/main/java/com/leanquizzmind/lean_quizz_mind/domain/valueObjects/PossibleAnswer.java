6d67d27ec71e96e6ab93481b56e06c252ddd9c60
Nicolas Vegas Rodriguez <alu0101321745@ull.edu.es>
2023-05-17 10:51:27 +0100

    refactor(PossibleAnswer): Refactor.

--- src/main/java/com/leanquizzmind/lean_quizz_mind/domain/valueObjects/PossibleAnswer.java
+++ src/main/java/com/leanquizzmind/lean_quizz_mind/domain/valueObjects/PossibleAnswer.java
@@ -2,43 +2,33 @@ package com.leanquizzmind.lean_quizz_mind.domain.valueObjects;
 
 import com.leanquizzmind.lean_quizz_mind.domain.models.Answer;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class PossibleAnswer {
 
-    private List<Answer> possibleAnswers = new ArrayList<>();
+    private final List<Answer> possibleAnswers;
 
     private PossibleAnswer(List<Answer> possibleAnswers) {
         this.possibleAnswers = possibleAnswers;
     }
 
     public static PossibleAnswer createPossibleAnswer(List<Answer> possibleAnswers) {
-
-        boolean answersNotExists = possibleAnswers.isEmpty();
-
-        if (answersNotExists) { throw new NullPointerException("The possibleAnswer List cannot be empty"); }
-
-        boolean isIncorrectFormat = haveBadFormat(possibleAnswers);
-
-        if (isIncorrectFormat) {
-            throw new IllegalArgumentException("Only one element can be the correct answer");
+        boolean answersAreEmpty = possibleAnswers.isEmpty();
+        if (answersAreEmpty) {
+            throw new NullPointerException("The possibleAnswer List cannot be empty");
         }
-
+        PossibleAnswer.checkOnlyOneCorrectAnswer(possibleAnswers);
         return new PossibleAnswer(possibleAnswers);
     }
 
-    private static boolean haveBadFormat(List<Answer> possibleAnswers) {
-
-        int haveBadFormat = Math.toIntExact(possibleAnswers.stream().filter(Answer::getCorrectAnswer).count());
-        return haveBadFormat != 1;
+    private static void checkOnlyOneCorrectAnswer(List<Answer> possibleAnswers) {
+        int correctAnswer = Math.toIntExact(possibleAnswers.stream().filter(Answer::getCorrectAnswer).count());
+        if (correctAnswer != 1) {
+            throw new IllegalArgumentException("Only one element can be the correct answer");
+        }
     }
 
     public List<Answer> getPossibleAnswers() {
         return possibleAnswers;
-    }
-
-    public void setPossibleAnswers(List<Answer> possibleAnswers) {
-        this.possibleAnswers = possibleAnswers;
     }
 }
